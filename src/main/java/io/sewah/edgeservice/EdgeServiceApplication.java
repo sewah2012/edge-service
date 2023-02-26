33b07bdb1f5dcd65ed74cffd41e3aefc4ed9ef36
Emmanuel Sahr Sewah <sewah2012@gmail.com>
2023-02-26 23:55:53 +0100

    update

--- src/main/java/io/sewah/edgeservice/EdgeServiceApplication.java
+++ src/main/java/io/sewah/edgeservice/EdgeServiceApplication.java
@@ -2,12 +2,20 @@ package io.sewah.edgeservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.context.annotation.Bean;
+import reactor.core.publisher.Mono;
 
 @SpringBootApplication
 public class EdgeServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EdgeServiceApplication.class, args);
+	}
+
+	@Bean
+	public KeyResolver keyResolver() {
+		return exchange -> Mono.just("anonymous");
 	}
 
 }
